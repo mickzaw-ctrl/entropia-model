70ac13007e280cde24184193a8b6763d3a72e6b7
Michał Ślusarczyk <mickzaw@gmail.com>
2026-08-21 05:26:00 +0200

    chore: bump version 5.0.0 -> 6.0.0 (ENTROPIA-6.0/R51) [2026-08-21T03:25:59Z]

--- entropia/__init__.py
+++ entropia/__init__.py
@@ -10,7 +10,7 @@ from .core import (operatory, superoperator, symuluj, entropia, czystosc,
                    LN2, GAMMA_A, GAMMA_B, DELTA_TAU, DELTA_S_Q)
 from .extensions import run_all
 
-__version__ = "5.0.0"
+__version__ = "6.0.0"
 __all__ = ["core", "extensions", "report", "run_all", "operatory",
            "superoperator", "symuluj", "entropia", "czystosc", "bloch",
            "delta_entropii", "zegar_stochastyczny", "liczby_kluczowe",
